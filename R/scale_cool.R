514e51495c0b6930b522da231877ce4ac23f3aac
Matthew J. Oldach <moldach686@gmail.com>
2019-02-14 23:33:34 -0500

    Add C O O L

--- R/scale_cool.R
+++ R/scale_cool.R
@@ -6,18 +6,32 @@ cool_palette <- c(
         ,"#FF6AD5"
 )
 
+#' Cool Colour and Fill Scales
+#'
+#' @seealso [ggplot2::scale_colour_discrete] [ggplot2:scale_fill_discrete]
+#' @inheritDotParams ggplot2::discrete_scale
+#' @name scale_cool
+#'
+#' @return the cool palette
+#' @export
 cool_pal <- function(){
         scales::manual_pal(cool_palette)
 }
 
+#' @rdname scale_cool
+#' @export
 scale_colour_cool <- function(...) {
         ggplot2::discrete_scale("colour", "cool", cool_pal(), ...)
 }
 
+#' @rdname scale_cool
+#' @export
 scale_color_cool <- function(){
         scale_colour_cool
 }
 
+#' @rdname scale_cool
+#' @export
 scale_fill_cool <- function(...) {
         ggplot2::discrete_scale('fill', 'cool', cool_pal(), ...)
 }
