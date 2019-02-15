3761663afd6f15d28ec06352fd9ac32c30db7857
Matthew J. Oldach <moldach686@gmail.com>
2019-02-14 22:50:30 -0500

    fix roxygen2 documentation

--- R/scale_avanti.R
+++ R/scale_avanti.R
@@ -7,30 +7,45 @@ avanti_palette <- c(
 )
 
 
-#' Avanti Colors and palette
+
+#' avanti palette
 #'
+#' @return avanti palette
 #' @export
+#'
 avanti_pal <- function(){
         scales::manual_pal(avanti_palette)
 }
+
 
 #' scale_colour_avanti
 #'
 #' @param ... pass me the palette
 #'
+#' @return scale_colour_avanti
 #' @export
+#'
 scale_colour_avanti <- function(...) {
         ggplot2::discrete_scale("colour", "avanti", avanti_pal(), ...)
 }
 
-scale_color_avanti <- scale_colour_avanti
+#' scale_color_avanti
+#'
+#' @return scale_color_avanti
+#' @export
+#'
+scale_color_avanti <- function(){
+        scale_colour_avanti
+}
 
 
 #' scale_fill_avanti
 #'
 #' @param ... pass me the palette
 #'
+#' @return
 #' @export
+#'
 scale_fill_avanti <- function(...) {
         ggplot2::discrete_scale('fill', 'avanti', avanti_pal(), ...)
 }
