316e632c156ddef45503d3ea321d7e5afed6cb5d
Matthew J. Oldach <moldach686@gmail.com>
2019-02-17 12:58:59 -0500

    Add package-level documentation

---
+++ R/vapoRwave-package.R
@@ -0,0 +1,24 @@
+#' vapoRwave: Add a vaporwave theme to ggplot2
+#' @description
+#' Giving a bit of 80's flair to ggplot2
+#'
+#' Vaporwave is an electronic music microgenre from the early 2010s.
+#' Its stylistic influences include chillwave, smooth jazz, chopped and screwed,
+#' new-age, and pop. To explain it in words kind of ruins the point.
+#' Vaporwave is the muzak that plays in an elevator in a mall in a
+#' futuristic Japanese cyberpunk dystopia. It’s the music sedated
+#' freaks listen to on the neo-dance floor. It’s the jams your funky
+#' smartphone mediates to when it’s running low on power and is cool
+#' with it. It’s a word that can’t be shouted, only coolly whispered.
+#' Vaporwave isn’t vaporware, it’s real.
+#'
+#' Vaporwave isn’t just something you listen to either, it’s something you
+#' experience, and experiences include visuals. Vaporwave visual art,
+#' simply referred to as “aesthetics,” is varied but tends to honor some
+#' core tenets. If you’re looking at something pink and teal with a marble
+#' classical bust and a glitchy Windows 95 logo, you’re probably looking at
+#' a vaporwave aesthetic
+#'
+#' @name vapoRwave
+#' @docType package
+NULL
